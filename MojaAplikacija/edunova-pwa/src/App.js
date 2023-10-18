709879600c77ef74792d448f8350670835b06515
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2023-10-18 13:58:06 +0200

    Kreiranje komponenata i instalacija bootstrap

--- MojaAplikacija/edunova-pwa/src/App.js
+++ MojaAplikacija/edunova-pwa/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Izbornik from './components/izbornik.component';
+import Pocetna from './components/pocetna.component';
+import NadzornaPloca from './components/nadzornaploca.component';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.js</code> Edunova.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Izbornik />
+      <Routes>
+        <Route path='/' element={<Pocetna/>} />
+        <Route path='/nadzornaploca' element={<NadzornaPloca />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+
---
+++ MojaAplikacija/edunova-pwa/src/components/nadzornaploca.component.js
@@ -0,0 +1,20 @@
+import React,  { Component } from "react";
+import { Container } from "react-bootstrap";
+
+
+export default class NadzornaPloca extends Component{
+    
+    
+    render(){
+        return (
+
+            <Container>
+                <p>Ovdje će doći lijepi graf</p>
+            </Container>
+            
+
+
+
+        );
+    }
+}
